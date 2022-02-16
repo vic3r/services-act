0cb31fd7d7504f417c13d32457bde8a0cc6909aa
vic3r <victor.g64r@gmail.com>
2022-02-16 10:58:48 -0600

    EventProcessor Impl for processing events

--- CommandsService/EventProcessing/EventProcessor.cs
+++ CommandsService/EventProcessing/EventProcessor.cs
@@ -1,10 +1,51 @@
+using System;
+using System.Text.Json;
+using AutoMapper;
+using CommandsService.Dtos;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace CommandsService.EventProcessing
 {
     public class EventProcessor : IEventProcessor
     {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(IServiceScopeFactory scopeFactory, IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+        
         public void ProcessEvent(string message)
         {
-            throw new System.NotImplementedException();
+            var eventType = DetermineEvent(message);
+
+            switch (eventType)
+            {
+                case EventType.PlatformPublished:
+                    // TODO: something
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            Console.WriteLine("--> Determining Event");
+
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    Console.WriteLine("--> Platform Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    Console.WriteLine("--> Could not determine event type");
+                    return EventType.Undetermined;
+            }
         }
     }
 
