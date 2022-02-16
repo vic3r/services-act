395be370cb2cc5c99f8bbd2077ce3f9c7ec95f84
vic3r <victor.g64r@gmail.com>
2022-02-16 10:39:51 -0600

    Add consumer for rabbitmq in commands svc

--- CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/Profiles/CommandsProfile.cs
@@ -12,6 +12,8 @@ namespace CommandService.Profiles
             CreateMap<Platform, PlatformReadDto>();
             CreateMap<CommandCreateDto, Command>();
             CreateMap<Command, CommandReadDto>();
+            CreateMap<PlatformPublishedDto, Platform>()
+                .ForMember(dest => dest.ExternalID, opt => opt.MapFrom(src => src.Id));
         }
     }
 }
